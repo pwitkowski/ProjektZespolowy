2403659cda5c2ff95bdafbb185916a862fd445f7
paweł <pawciowit@gmail.com>
2015-03-20 23:28:04 +0100

    usunięcie napisu na starcie

--- ProjektZespolowy/Assets/Done/DoneScripts/UnappliedScripts/guiTextSet.cs
+++ ProjektZespolowy/Assets/Done/DoneScripts/UnappliedScripts/guiTextSet.cs
@@ -6,7 +6,7 @@ public class guiTextSet : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	 GetComponent<GUIText>().text= 	"\nArrow Keys: Move\nLeft Shift: Sneak\nZ: Use Switch\nX: Attract Attention";
-	
+	 //TODO tutaj można dodać wskaźniki
 	}
 
 }
