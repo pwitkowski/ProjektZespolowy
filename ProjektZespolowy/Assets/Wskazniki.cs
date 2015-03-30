e27d5c1d364c29e2d921a2d64a57eea356a15dad
pwitkowski <pawciowit@gmail.com>
2015-03-30 22:06:26 +0200

    Dodanie timera

--- ProjektZespolowy/Assets/Wskazniki.cs
+++ ProjektZespolowy/Assets/Wskazniki.cs
@@ -5,14 +5,26 @@ using UnityEngine.UI;
 public class Wskazniki : MonoBehaviour {
 
 	public Text napisIloscIteracji;
+	public Slider suwakCzasu;
+	public float licznikCzasu = 5f;
+	public static bool restartGry = false;
+
+	private float czas;
 
 	// Use this for initialization
 	void Start () {
+		czas = licznikCzasu;
 		napisIloscIteracji.text = ""+Gra.iloscIteracji;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		licznikCzasu -= Time.deltaTime;
+		if (licznikCzasu > 0) {
+			suwakCzasu.value = (100 * licznikCzasu) / czas;
+		}else{
+			Gra.iloscIteracji++;
+			Application.LoadLevel (0);
+		}
 	}
 }
