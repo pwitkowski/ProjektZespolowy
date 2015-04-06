d4d074306611dcb06bf27b270f3da58e15444a1c
Grzegorz S <malpa447@gmail.com>
2015-04-06 10:02:08 +0200

    Znikanie baterii po naladowaniu wskaznika

--- ProjektZespolowy/Assets/InterakcjaObiektyWskazniki.cs
+++ ProjektZespolowy/Assets/InterakcjaObiektyWskazniki.cs
@@ -24,6 +24,7 @@ public class InterakcjaObiektyWskazniki : MonoBehaviour
 			case "LadowanieBaterii":
 				if (Gra.bateria < 100) {
 					Gra.bateria += (100 - Gra.bateria);
+					Destroy(gameObject);
 				}
 				break;
 			}
